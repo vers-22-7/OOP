3a74e876ab03f86dd4596d8f47840e454e8dba1f
vers-22-7 <mkulachuk@gmail.com>
2024-09-10 10:55:25 +0300

    pr2 tr 2

---
+++ practical 2/practical2_task1/practical2_task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+//9 Користувач вводить 2 числа, вивести те з них, для якого залишок від ділення на 9 буде найбільшим.
+namespace dILENAnA9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть перше число: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Введіть друге число: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int CHISLONA9_1 = num1 % 9;
+            int CHISLONA9_2 = num2 % 9;
+
+            if (CHISLONA9_1 > CHISLONA9_2)
+            {
+                Console.WriteLine("Число з більшим залишком від ділення на 9: " + num1);
+            }
+            else if (CHISLONA9_1 > CHISLONA9_2)
+            {
+                Console.WriteLine("Число з більшим залишком від ділення на 9: " + num2);
+            }
+            else
+            {
+                Console.WriteLine("Обидва числа мають однаковий залишок від ділення на 9");
+            }
+        }
+    }
+}
---
+++ practical 2/practical2_task2/practical2_task2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+//Гра в кості. Програма та гравець викидають кості від 1 до 6 протягом 5 раундів. Виграє той, хто набрав найбільшу суму.  Вивести кожен раунд та переможця.
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random chislo = new Random();
+            int[] RollsP = new int[5];
+            int[] RollsPC = new int[5];
+            int P = 0;
+            int PC = 0;
+            int round = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                RollsP[i] = chislo.Next(1, 7);
+                RollsPC[i] = chislo.Next(1, 7);
+            }
+            while (true)
+            {
+                if (round >= 5)
+                    break;
+                Console.WriteLine($"Раунд {round + 1}: Гравець викинув {RollsP[round]}, Комп'ютер викинув {RollsPC[round]}");
+                if (RollsP[round] > RollsPC[round])
+                {
+                    Console.WriteLine("Гравець виграв цей раунд!");
+                    P++;
+                }
+                else if (RollsPC[round] > RollsP[round])
+                {
+                    Console.WriteLine("Комп'ютер виграв цей раунд!");
+                    PC++;
+                }
+                else
+                {
+                    Console.WriteLine("Нічия в цьому раунді!");
+                }
+                round++;
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Загальний рахунок: Гравець {P}, Комп'ютер {PC}");
+            if (P > PC)
+            {
+                Console.WriteLine("Гравець виграв гру!");
+            }
+            else if (PC > P)
+            {
+                Console.WriteLine("Комп'ютер виграв гру!");
+            }
+            else
+            {
+                Console.WriteLine("Гра закінчилася внічию!");
+            }
+        }
+
+    }
+}
