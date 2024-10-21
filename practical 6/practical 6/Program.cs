251d29caeb0df32cca9c6a0800ecadcf569d362c
vers-22-7 <mkulachuk@gmail.com>
2024-10-21 17:00:17 +0300

    pr6

---
+++ practical 6/practical 6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Animal
+{
+    private string name;
+    private int age;
+    private string species;
+
+    public Animal(string name, int age, string species)
+    {   this.name = name;
+        this.age = age;
+        this.species = species;}
+
+    // Метод для виведення даних про тварину
+    public void DisplayInfo(){
+        Console.WriteLine($"Ім'я тварини: {name}");
+        Console.WriteLine($"Вік тварини: {age} років");
+        Console.WriteLine($"Вид тварини: {species}");}
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {   Animal myAnimal = new Animal("Барсик", 5, "Кіт");
+        myAnimal.DisplayInfo();}
+}
